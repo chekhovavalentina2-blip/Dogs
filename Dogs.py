80d8a6bb92c868e97c83419c4db2cd5997e62668
chekhovavalentina2-blip <chekhovavalentina2@gmail.com>
2025-08-26 09:00:06 +0300

    +

--- Dogs.py
+++ Dogs.py
@@ -2,8 +2,31 @@ from tkinter import *
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
+from tkinter import messagebox as mb
 
+
+def show_image():
+    image_url = get_dog_image()
+    if image_url:
+        try:
+            response = requests.get(image_url)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img.thumbnail((300, 300))
+            label.config(image=img)
+            label.image = img
+        except Exception as e:
+            mb.showerror("Ошибка", f"Возникла ошибка: {e}")
 
 window = Tk()
 window.title("Картинки с собачками")
 window.geometry("360x420")
+
+label = Label()
+label.pack(pady=10)
+
+button = Button(text="Загрузить изображение", command=show_image)
+button.pack(pady=10)
+
+window.mainloop()
